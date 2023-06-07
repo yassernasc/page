bf1eb64daebbddaee18c8aa3132e76d81f567c5b
Yasser Nascimento <dev@yasser.email>
2023-06-07 15:42:17 -0300

    use markdown

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,11 @@
 import { defineConfig } from 'astro/config'
 import css from 'unocss/astro'
 
-export default defineConfig({ integrations: [css()] })
+const setDefaultLayout = () => (tree, file) => {
+  file.data.astro.frontmatter.layout = '../layout.astro'
+}
+
+export default defineConfig({
+  integrations: [css()],
+  markdown: { remarkPlugins: [setDefaultLayout] },
+})
