5eca6b954e3776b3a09b95ef66753977e7532964
Yasser Nascimento <dev@yasser.email>
2023-07-18 19:22:32 -0300

    customize unocss typography

--- uno.config.mjs
+++ uno.config.mjs
@@ -1,5 +1,15 @@
 import { defineConfig, presetTypography, presetUno } from 'unocss'
 
 export default defineConfig({
-  presets: [presetUno({ dark: 'media' }), presetTypography()],
+  presets: [
+    presetUno({ dark: 'media' }),
+    presetTypography({
+      cssExtend: {
+        h1: { 'margin-bottom': '2em' },
+        h2: { 'font-size': '1.2em' },
+        h3: { 'font-size': '1em' },
+        h4: { 'font-size': '0.8em' },
+      },
+    }),
+  ],
 })
