9f0411e9a3b9e3a0a5b00e5aa2173bc11b1064bc
Yasser Nascimento <dev@yasser.email>
2023-07-27 10:23:00 -0300

    refine devs page, add new links

--- uno.config.mjs
+++ uno.config.mjs
@@ -9,6 +9,7 @@ export default defineConfig({
         h2: { 'font-size': '1.2em' },
         h3: { 'font-size': '1em' },
         h4: { 'font-size': '0.8em' },
+        blockquote: { 'margin-bottom': '2em' },
       },
     }),
   ],
