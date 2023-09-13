bb105e73c371c3de688d0645ed5d27fdfca1a465
Yasser Nascimento <dev@yasser.email>
2023-09-13 13:21:15 -0300

    structure css

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,14 @@
 import { defineConfig } from 'astro/config'
 
 const setDefaultLayout = () => (tree, file) => {
-  const isPage = file.data.astro.frontmatter.page_title !== undefined
-  if (isPage) {
+  const { frontmatter } = file.data.astro
+  const isNote = !!frontmatter.subject
+
+  if (isNote) {
+    file.data.astro.frontmatter.layout = '@layouts/note.astro'
+    file.data.astro.frontmatter.page_title = `${frontmatter.subject} notes`
+    file.data.astro.frontmatter.description = `${frontmatter.subject} snippets and notes`
+  } else {
     file.data.astro.frontmatter.layout = '@layouts/base.astro'
   }
 }
